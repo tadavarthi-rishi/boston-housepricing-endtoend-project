3dccb1f462205439c363fdf7dd685daed3b894b9
Rishi <rtadavar@asu.edu>
2023-06-08 14:08:05 -0400

    this is the commit for web application

--- README.md
+++ README.md
@@ -6,3 +6,10 @@
 2. [VscodeIDE](https://code.visualstudio.com)
 3. [HerokuAccount](https://heroku.com)
 4. [GitCLI](https://git-scm.com/book/en/v2/Getting-Started-The-Command-Line)
+
+
+Create a new environment
+
+- - -  
+conda create -p venv python==3.7 -y
+- - - 
---
+++ app.py
@@ -0,0 +1,30 @@
+import pickle
+from flask import Flask, request, app, jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__) #here __name__ is starting point of flask app
+
+#loading the pickle file into model
+regmodel = pickle.load(open('regmodel.pkl','rb'))
+scalar = pickle.load(open('scaling.pkl','rb'))
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data = request.json['data']
+    print(data)
+    new_data = scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    output = regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+    
+    
+    
+    
